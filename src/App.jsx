029fb9cdd0c4294ef1788677833a6368ab1f8f63
Kei <motosjairo12@gmail.com>
2024-06-11 13:37:05 +0200

    Segunda pausa

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,89 @@
+import { useState } from "react";
 import Sidebar from "./components/shared/Sidebar"
+import { RiMenu3Fill, RiUser3Line, RiAddLine, RiPieChartLine, RiCloseLine, RiSearch2Line, RiArrowDownSLine } from "react-icons/ri";
+
 
 
 function App() {
 
+  const [showMenu, setSetshowMenu] = useState(false);
+  const [showOrder, setSetshowOrder] = useState(false);
+
+  const toggleMenu = () => {
+    setSetshowMenu(!showMenu)
+  }
+
   return (
     <div className="bg-[#262837] w-full min-h-screen">
-      <Sidebar/>
+      <Sidebar showMenu={showMenu} />
+      {/* menu movil */}
+      <nav className="bg-[#1F1D2B] lg:hidden fixed w-full bottom-0 left-0 text-3xl text-gray-400 py-2 px-8 flex items-center justify-between rounded-tl-xl rounded-tr-xl">
+        <button className="p-2">
+          <RiUser3Line/>
+        </button>
+        <button className="p-2">
+          <RiAddLine/>
+        </button>
+        <button className="p-2">
+          <RiPieChartLine/>
+        </button>
+        <button 
+          onClick={toggleMenu}  
+          className="text-white p-2"
+        >
+          { showMenu ? <RiCloseLine/> : <RiMenu3Fill/> }
+        </button>
+      </nav>
+      <main className="lg:pl-28 grid grid-cols-1 lg:grid-cols-8">
+        <div className="lg:col-span-6">
+          {/* Header */}
+          <header className="p-4">
+            {/* Title and Search */}
+            <div className="flex flex-col md:flex-row md:justify-between md:items-center gap-4 mb-6">
+              <div>
+                <h1 className="text-2xl text-gray-300">Jeager Resto</h1>
+                <p className="text-gray-500">07 octubre 2022</p>
+              </div>
+              <form>
+                <div className="w-full relative">
+                  <RiSearch2Line className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-300"/>
+                  <input 
+                    type="text"
+                    className="bg-[#1F1D2B] w-full py-2 pl-10 pr-4 rounded-lg text-gray-300 outline-none"
+                    placeholder="Search dish..."
+                  />
+                </div>
+              </form>
+            </div>
+            {/* Tabs */}
+            <nav className="text-gray-300 flex items-center justify-between md:justify-start md:gap-8 border-b mb-6">
+              <a href="#" className="relative py-2 pr-4 before:w-1/2 before:h-1 before:absolute before:bg-[#ec7c6a] before:l-0 before:rounded-full before:-bottom-[2px] text-[#ec7c6a]">
+                Hot dishes
+              </a>
+              <a href="#" className="py-2 pr-4">
+                Cold dishes
+              </a>
+              <a href="#" className="py-2 pr-4">
+                Soup
+              </a>
+              <a href="#" className="py-2">
+                Grill
+              </a>
+            </nav>
+            {/* Title content */}
+            <div className="flex items-center justify-between">
+              <h2 className="text-xl text-gray-300">Choose Dishes</h2>
+              <button className="flex items-center gap-4 text-gray-300 bg-[#1F1D2B] py-2 px-4 rounded-lg">
+                <RiArrowDownSLine /> Dine in
+              </button>
+            </div>
+          </header>
+        </div>
+        <div className="lg:col-span-2 fixed lg:static right-0">Carrito</div>
+      </main>
     </div>
   )
 }
+
 
 export default App
