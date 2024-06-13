0e72d5cb2c2877376445c27ef38fdcaa1da4d423
Kei <motosjairo12@gmail.com>
2024-06-13 11:09:34 +0200

    Last commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,31 @@
 import { useState } from "react";
+import { RiMenu3Fill, RiUser3Line, RiAddLine, RiPieChartLine, RiCloseLine, RiArrowDownSLine } from "react-icons/ri";
 import Sidebar from "./components/shared/Sidebar"
-import { RiMenu3Fill, RiUser3Line, RiAddLine, RiPieChartLine, RiCloseLine, RiSearch2Line, RiArrowDownSLine } from "react-icons/ri";
+import Car from "./components/shared/Car";
+import Header from "./components/shared/Header";
+import Card from "./components/shared/Card";
+
 
 
 
 function App() {
 
-  const [showMenu, setSetshowMenu] = useState(false);
-  const [showOrder, setSetshowOrder] = useState(false);
+  const [showMenu, setShowMenu] = useState(false);
+  const [showOrder, setShowOrder] = useState(false);
 
   const toggleMenu = () => {
-    setSetshowMenu(!showMenu)
+    setShowMenu(!showMenu);
+    setShowOrder(false);
+  }
+  const toggleOrder = () => {
+    setShowOrder(!showOrder);
+    setShowMenu(false);
   }
 
   return (
     <div className="bg-[#262837] w-full min-h-screen">
       <Sidebar showMenu={showMenu} />
+      <Car showOrder={showOrder} setShowOrder={setShowOrder} />
       {/* menu movil */}
       <nav className="bg-[#1F1D2B] lg:hidden fixed w-full bottom-0 left-0 text-3xl text-gray-400 py-2 px-8 flex items-center justify-between rounded-tl-xl rounded-tr-xl">
         <button className="p-2">
@@ -24,7 +34,10 @@ function App() {
         <button className="p-2">
           <RiAddLine/>
         </button>
-        <button className="p-2">
+        <button 
+          onClick={toggleOrder}
+          className="p-2"
+        >
           <RiPieChartLine/>
         </button>
         <button 
@@ -34,52 +47,29 @@ function App() {
           { showMenu ? <RiCloseLine/> : <RiMenu3Fill/> }
         </button>
       </nav>
-      <main className="lg:pl-28 grid grid-cols-1 lg:grid-cols-8">
-        <div className="lg:col-span-6">
+      <main className="lg:pl-32 lg:pr-96 pb-20">
+        <div className="md:p-8 p-4">
           {/* Header */}
-          <header className="p-4">
-            {/* Title and Search */}
-            <div className="flex flex-col md:flex-row md:justify-between md:items-center gap-4 mb-6">
-              <div>
-                <h1 className="text-2xl text-gray-300">Jeager Resto</h1>
-                <p className="text-gray-500">07 octubre 2022</p>
-              </div>
-              <form>
-                <div className="w-full relative">
-                  <RiSearch2Line className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-300"/>
-                  <input 
-                    type="text"
-                    className="bg-[#1F1D2B] w-full py-2 pl-10 pr-4 rounded-lg text-gray-300 outline-none"
-                    placeholder="Search dish..."
-                  />
-                </div>
-              </form>
-            </div>
-            {/* Tabs */}
-            <nav className="text-gray-300 flex items-center justify-between md:justify-start md:gap-8 border-b mb-6">
-              <a href="#" className="relative py-2 pr-4 before:w-1/2 before:h-1 before:absolute before:bg-[#ec7c6a] before:l-0 before:rounded-full before:-bottom-[2px] text-[#ec7c6a]">
-                Hot dishes
-              </a>
-              <a href="#" className="py-2 pr-4">
-                Cold dishes
-              </a>
-              <a href="#" className="py-2 pr-4">
-                Soup
-              </a>
-              <a href="#" className="py-2">
-                Grill
-              </a>
-            </nav>
-            {/* Title content */}
-            <div className="flex items-center justify-between">
-              <h2 className="text-xl text-gray-300">Choose Dishes</h2>
-              <button className="flex items-center gap-4 text-gray-300 bg-[#1F1D2B] py-2 px-4 rounded-lg">
-                <RiArrowDownSLine /> Dine in
-              </button>
-            </div>
-          </header>
+          <Header />
+          {/* Title content */}
+          <div className="flex items-center justify-between mb-16">
+            <h2 className="text-xl text-gray-300">Choose Dishes</h2>
+            <button className="flex items-center gap-4 text-gray-300 bg-[#1F1D2B] py-2 px-4 rounded-lg">
+              <RiArrowDownSLine /> Dine in
+            </button>
+          </div>
+          {/* Content */}
+          <div className="p-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-16">
+            {/* Card */}
+            <Card img='Curry.png' description='Spicy Curry' price='$2.29' inventory='20' />
+            <Card img='kimchi.png' description='Kimchi family' price='$4.79' inventory='13' />
+            <Card img='Curry.png' description='Spicy Curry' price='$2.29' inventory='20' />
+            <Card img='Sushi.png' description='Sushi Fresh' price='$74.09' inventory='2' />
+            <Card img='Curry.png' description='Spicy Curry' price='$2.29' inventory='20' />
+            <Card img='kimchi.png' description='Kimchi family' price='$4.79' inventory='13' />
+          </div>
         </div>
-        <div className="lg:col-span-2 fixed lg:static right-0">Carrito</div>
+        
       </main>
     </div>
   )
